30f82d31a21eb01191e09eef4dfd296288efb007
Jeon Minjae <mjjeon@shinwoovalve.com>
2026-05-26 15:55:59 +0900

    chore: remove legacy scripts and unused tools

--- scripts/check_staging.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-require('dotenv').config({ path: '.env.local' });
-const client = new Client({ connectionString: process.env.STAGING_DB_URL });
-client.connect()
-  .then(() => client.query('SELECT * FROM weekly_reports ORDER BY "createdDate" DESC LIMIT 5'))
-  .then(res => { console.log(JSON.stringify(res.rows, null, 2)); process.exit(0); })
-  .catch(err => { console.error(err); process.exit(1); });
--- 서버상태확인.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 >nul
-node check_status.js
-echo.
-pause
